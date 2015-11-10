e97c56f38fe629e10ed47c92285ad13b959757db
Daniel Carral <dan@dcarral.org>
2015-11-10 01:31:39 +0100

    Add answer to exercise 4-2 (a)

--- chapter4.hs
+++ chapter4.hs
@@ -9,3 +9,15 @@
 halve :: [a] -> ([a], [a])
 halve xs = (take half xs, drop half xs)
   where half = length xs `div` 2
+
+{- Exercise 2:
+  Consider a function safetail :: [a] -> [a] that behaves as the library function tail, except that safetail maps the empty list to itself, whereas tail produces an error in this case. Define safetail using:
+
+  (a) a conditional expression;
+  (b) guarded equations;
+  (c) pattern matching
+-}
+
+-- (a) Using a conditional expression:
+safetail_conditional :: [a] -> [a]
+safetail_conditional xs = if null xs then xs else tail xs
